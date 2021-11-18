a79ad363c0b20ef80632c7ee1f21f0ca4c75a18b
Nickel-Angel <1239004072@qq.com>
2021-11-18 14:37:39 +0800

    binary tree

---
+++ Discrete Math/binary tree/binary tree/binary tree.cpp
@@ -0,0 +1,30 @@
+﻿#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+const int maxn = 1e5 + 10;
+int n, a[maxn];
+
+int main()
+{
+    scanf_s("%d", &n);
+    for (int i = 1; i <= n; ++i)
+        scanf_s("%d", a + i);
+    std::sort(a + 1, a + n + 1);
+    for (int i = 1; i < n; ++i)
+    {
+        for (int j = i; j <= n; ++j)
+            printf("%d%c", a[j], " \n"[j == n]);
+        a[i + 1] += a[i];
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (a[j] > a[j + 1])
+                std::swap(a[j], a[j + 1]);
+            else
+                break;
+        }
+    }
+    printf("%d\n", a[n]);
+    return 0;
+}
